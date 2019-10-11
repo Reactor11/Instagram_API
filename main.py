6d67aa31e77a365c02fb128aba3373c8bea64c90
Manas Bhardwaj <manas1109bhardwaj@gmail.com>
2019-10-12 04:11:16 +0530

    Added followings,posts feature.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from time import sleep
 import pandas as pd
 import urllib.request, urllib.parse, urllib.error
@@ -11,8 +10,10 @@ fname_list = list(df.iloc[:,0])
 fname_list = list(map(str.strip, fname_list))
 
 f_list = list()
+flg_list = list()
+post_list = list()
 # print(fname_list)
-count = 0 
+count = 0
 error = []
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -27,16 +28,23 @@ for i in range(len(fname_list)):
         for link in soup.find_all('meta',property='og:description'):
             a = link.get('content')
         a = a.split(" ")
-        if(a[1] == 'Followers,'):
+        if(a[1] == 'Followers,' and a[3] == 'Following,' and a[5] == 'Posts'):
             follower = a[0]
+            following = a[2]
+            posts = a[4]
             f_list.append(a[0])
-        print(f"User Name : {fname_list[i]} \t No. of follower = {follower}")
+            flg_list.append(a[2])
+            post_list.append(a[4])
+
+        print(f"User Name : {fname_list[i]} \t No. of follower's = {follower} \t No. of following's = {following} \t No. of Posts = {posts}" )
         if(count%10 == 0):
             print('\n\nDo no close the window \n\n')
             sleep(5)
     except:
         print("User Name :",fname_list[i].strip(),"Not found")
-        f_list.append('Wrong Username')
+        f_list.append('n/a')
+        flg_list.append('n/a')
+        post_list.append('n/a')
         error.append(fname_list[i].strip())
         continue
 
@@ -45,8 +53,9 @@ if(len(error) != 0):
     print("Username's not retrived due to error :")
     [print(f'\t{i}') for i in error]
 print('\n\n\nSaving data to IG_Retrived.xlsx...')
-#for i in
 q_list = list()
+f_temp = list()
+
 for i in f_list:
     if(i[-1] == 'm'):
         a = float(i[:-1]) * 1000000
@@ -56,8 +65,32 @@ for i in f_list:
         q_list.append(a)
     else:
         q_list.append(i)
-df['Count'] = q_list
-df.to_excel("IG_Retrived.xlsx")
+
+for i in flg_list:
+    if(i[-1] == 'm'):
+        a = float(i[:-1]) * 1000000
+        f_temp.append(a)
+    elif(i[-1] == 'k'):
+        a = float(i[:-1]) * 1000
+        f_temp.append(a)
+    else:
+        f_temp.append(i)
+
+post_temp = list()
+for i in post_list:
+    if(i[-1] == 'm'):
+        a = float(i[:-1]) * 1000000
+        post_temp.append(a)
+    elif(i[-1] == 'k'):
+        a = float(i[:-1]) * 1000
+        post_temp.append(a)
+    else:
+        post_temp.append(i)
+
+df['Followers'] = q_list
+df['Followings'] = f_temp
+df['Posts'] = post_temp
+df.to_excel("IG_Retrived.xlsx",index=False)
 sleep(5)
 print("Data Saved.\nPress enter key to exit...")
 sleep(2)
